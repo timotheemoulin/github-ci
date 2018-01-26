9c67c3f80e41f675b5655fb92253c0b74e82cb40
Timothée Moulin <timothee.moulin@virtua.ch>
2018-01-26 21:23:04 +0100

    full changes

--- README.fr.md
+++ README.fr.md
@@ -8,10 +8,10 @@
 
 [English version here](README.md)
 
-Etat de la version [![Build Status](https://travis-ci.org/timotheemoulin/github-ci.svg?branch=master)](https://travis-ci.org/timotheemoulin/github-ci)
+État de la version [![Build Status](https://travis-ci.org/timotheemoulin/github-ci.svg?branch=master)](https://travis-ci.org/timotheemoulin/github-ci)
 
 ## Aspell
 
 Aspell est un correcteur orthographique. Vous pouvez l'utiliser pour vérifier vos fichiers *markdown* et ainsi vous assurer que votre documentation ne contient pas d'erreur.
 
-Vous pouvez écrire en anglais dans les fichiers en français mais l'inverse n'est pas possible. Une seule langue par fichier n'est autorisée.
+Vous pouvez écrire en anglais dans les fichiers en français mais l'inverse n'est pas possible. Une seule langue par fichier n'est autorisée en addition de l'anglais.
--- bin/spell-check.sh
+++ bin/spell-check.sh
@@ -7,7 +7,9 @@ BLUE='\033[0;36m'
 NC='\033[0m' # No Color
 
 LANGUAGE_KEY='#Language: '
-AVAILABLE_LANGUAGES='fr|en|de'
+# All available languages aspell packages must be loaded from the .travis.yml file
+# addons.apt.packages: [..., aspell-{lang}]
+AVAILABLE_LANGUAGES='fr|en'
 
 # Allow executing this file without being in the travis execution
 if [ -z "$TRAVIS_COMMIT_RANGE" ]; then
@@ -32,39 +34,57 @@ echo -e "$BLUE>> Following markdown files were changed (commit range: $TRAVIS_CO
 echo "$CHANGED_FILES"
 echo -e "$BLUE>> Text will be checked without metadata, html, and links:$NC"
 
-FOUND_LANGUAGES=`echo "$CHANGED_FILES" | xargs cat | grep "$LANGUAGE_KEY" | sed -E "s/$LANGUAGE_KEY($AVAILABLE_LANGUAGES)/\1/g"`
-echo -e "$BLUE>> Languages recognized from the metadata:$NC"
-echo "$FOUND_LANGUAGES"
+# Check each file independently as each file has its own language
+for CURRENT_FILE in $CHANGED_FILES; do
 
-while read LINE; do
-    if [ "$LINE" != "en" ]
-    then
-        FILE_LANGUAGE="$LINE"
+    FOUND_LANGUAGES=`echo "$CURRENT_FILE" | xargs cat | grep "$LANGUAGE_KEY" | sed -E "s/$LANGUAGE_KEY($AVAILABLE_LANGUAGES)/\1/g"`
+    echo -e "$BLUE>> Languages recognized from the metadata:$NC"
+    echo "$FOUND_LANGUAGES"
 
+    while read LINE; do
+        if [ "$LINE" != "en" ]
+        then
+            FILE_LANGUAGE="$LINE"
+
+        fi
+    done <<< "$FOUND_LANGUAGES"
+
+    if [ -z "$FILE_LANGUAGE" ]; then
+        FILE_LANGUAGE='en'
     fi
-done <<< "$FOUND_LANGUAGES"
 
-if [ -z "$FILE_LANGUAGE" ]; then
-    FILE_LANGUAGE='en'
-fi
-
-# Get the content of all markdown files that changed
-CHANGED_CONTENT=`cat $(echo "$CHANGED_FILES" | sed -E ':a;N;$!ba;s/\n/ /g')`
-# Remove code blocks
-CHANGED_CONTENT=`echo "$CHANGED_CONTENT" | sed  -n '/^\`\`\`/,/^\`\`\`/ !p'`
-# Remove links
-CHANGED_CONTENT=`echo "$CHANGED_CONTENT" | sed -E 's/\]((\w*)\)//g'`
+    # Get the content of all markdown files that changed
+    CHANGED_CONTENT=`cat $(echo "$CURRENT_FILE" | sed -E ':a;N;$!ba;s/\n/ /g')`
+    # Remove code blocks
+    CHANGED_CONTENT=`echo "$CHANGED_CONTENT" | sed  -n '/^\`\`\`/,/^\`\`\`/ !p'`
+    # Remove links
+    CHANGED_CONTENT=`echo "$CHANGED_CONTENT" | sed -E 's/http(s)?:\/\/([^ ]+)//g'`
 
 
-# Check content spelling in English
-echo -e "$BLUE>> Checking in English (many technical words are in English anyway)...$NC"
-MISSPELLED=`echo "$CHANGED_CONTENT" | aspell --lang=en --personal=./.aspell/aspell.en.pws list | sort -u`
+    # Check content spelling in English
+    echo -e "$BLUE>> Checking in English (many technical words are in English anyway)...$NC"
+    MISSPELLED_EN=`echo "$CHANGED_CONTENT" | aspell --lang=en --personal=./.aspell/aspell.en.pws list | sort -u`
+    echo "$MISSPELLED_EN" | sed -E ':a;N;$!ba;s/\n/, /g'
+    # Check content spelling in the detected language
+    if [ "$FILE_LANGUAGE" != "en" ]; then
+        echo -e "$BLUE>> Checking in '$FILE_LANGUAGE' too...$NC"
+        MISSPELLED_LANG=`echo "$CHANGED_CONTENT" | aspell --lang=$FILE_LANGUAGE --personal=./.aspell/aspell.$FILE_LANGUAGE.pws list | sort -u`
+        echo "$MISSPELLED_LANG" | sed -E ':a;N;$!ba;s/\n/, /g'
+    fi
 
-# Check content spelling in the detected language
-if [ "$FILE_LANGUAGE" != "en" ]; then
-    echo -e "$BLUE>> Checking in '$FILE_LANGUAGE' too..."
-    MISSPELLED=`echo "$MISSPELLED" | aspell --lang=$FILE_LANGUAGE --personal=./.aspell/aspell.$FILE_LANGUAGE.pws list | sort -u`
-fi
+done
+
+echo -e "filter matching"
+# Only cry if there is an unknown word in English and the specific language of the document
+MISSPELLED=()
+for WORD_EN in "${MISSPELLED_EN[@]}"; do
+    echo -e "en"
+    for WORD in "${MISSPELLED_LANG[@]}"; do
+        if [[ WORD_EN = $WORD ]]; then
+            MISSPELLED+=("$MISSPELLED_EN")
+        fi
+    done
+done
 
 if [ -n "$MISSPELLED" ] ; then
 	NB_MISSPELLED=`echo "$MISSPELLED" | wc -l`
