a2b88e9ad9e605e16f5edd8e13364c1c05aadfba
Timothée Moulin <timothee.moulin@virtua.ch>
2018-01-27 23:30:27 +0100

    add rasterize and phantomjs

---
+++ .phantom/rasterize.sh
@@ -0,0 +1,14 @@
+#!/usr/bin/env bash
+inputfile=$1
+outputfile=$2
+cssfile=/data/.phantom/rasterize.css
+tmpfile=/tmp/markdown2pdf${outputfile##*/}.html
+ 
+echo "<html><head>" > $tmpfile
+echo "<link rel=\"stylesheet\" type=\"text/css\" href=\"/data/.phantom/github.css\">" >> $tmpfile
+echo "<link rel=\"stylesheet\" type=\"text/css\" href=\"$cssfile\">" >> $tmpfile
+echo "</head><body>" >> $tmpfile
+markdown $inputfile | iconv -f utf8 -t l1 >> $tmpfile
+echo "</body></html>" >> $tmpfile
+phantomjs /data/.phantom/rasterize.js $tmpfile $outputfile A4
+rm $tmpfile
---
+++ README.md
@@ -0,0 +1,24 @@
+# This is a basic README
+
+It is written in Markdown.
+
+- You can add some lists
+ - Even on multilevels
+ - That works well
+
+## You can add subtitles
+
+> You can quote text
+
+Or even add some **bold** and *italic* 
+
+You can add inline `code`
+
+```
+Or
+write
+a
+multiline
+code
+block
+```
