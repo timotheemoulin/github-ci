a427a9428b7da34914a907a563f841d459d496f0
Timothée Moulin <timothee.moulin@virtua.ch>
2018-02-04 17:28:25 +0100

    try running docker from an external script

--- .phantom/run.sh
+++ .phantom/run.sh
@@ -1,6 +1,7 @@
 #!/usr/bin/env bash
 
+set -x
+
 docker-compose -f docker-compose.yml build
 docker-compose -f docker-compose.yml run phantom .phantom/rasterize.sh README.md .phantom/build/README.pdf
-docker-compose -f docker-compose.yml run phantom touch /tmp/foo
-docker-compose -f docker-compose.yml run phantom ls -la /tmp/
+docker-compose -f docker-compose.yml run phantom ls -la /
