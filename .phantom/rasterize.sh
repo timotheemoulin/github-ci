cbe962bace02a0efb6db8df70ddbbc951e7c078e
Timothée Moulin <timothee.moulin@virtua.ch>
2018-02-05 22:15:13 +0100

    refactor paths

--- .phantom/rasterize.sh
+++ .phantom/rasterize.sh
@@ -2,25 +2,30 @@
 
 set -x
 
+currentpath="$( cd "$(dirname "$0")" ; pwd -P )"
+
+# in and out files are relative to the calling path
 inputfile=$1
 outputfile=$2
-cssfile=.phantom/rasterize.css
+
+# css and js files are relative to the current script
+cssfile=$currentpath/rasterize.css
+
+# tmp file can be anywhere
 tmpfile=/tmp/markdown2pdf${outputfile##*/}.html
- 
+
+# echo the basic html structure and add some nice css to it 
 echo "<html><head>" > $tmpfile
 echo "<link rel=\"stylesheet\" type=\"text/css\" href=\"github.css\">" >> $tmpfile
 echo "<link rel=\"stylesheet\" type=\"text/css\" href=\"$cssfile\">" >> $tmpfile
 echo "</head><body>" >> $tmpfile
+
 # Latin1 works with iso-8859-1 (.js)
 markdown $inputfile | iconv -f utf8 -t l1 >> $tmpfile
 echo "</body></html>" >> $tmpfile
 
-phantomjs .phantom/rasterize.js $tmpfile $outputfile A4
+# convert our html version of markdown to pdf
+phantomjs $currentpath/rasterize.js $tmpfile $outputfile A4
 
-pwd
-
-find . -name "*README*"
-
-find / -name "*README*"
-
+# clean the mess
 rm $tmpfile
